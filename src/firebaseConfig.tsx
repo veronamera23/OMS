de5dbb3304b1c82b0be6454a9483c75d624da142
Shaina <sdtalisay@up.edu.ph>
2024-10-31 12:18:03 +0800

    connected sign up authentication [Shaina]

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword, Auth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,6 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // Initialize Firestore
+const db = getFirestore(app); 
 
-export { app, db }; // Export db for use in other components
+export const auth= getAuth();
+export { app, db }; 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import Example from './components/Example';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import Navbar from './components/Navbar';
+import App from './App'; 
+import Register from './components/register';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,6 +14,7 @@ root.render(
   <React.StrictMode>
     <Navbar />
     <Example />
+    <Register/>
   </React.StrictMode>
 );
 
@@ -19,3 +22,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export{};
