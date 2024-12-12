9e31060ca596a8273d5a940b8a69b39f06901de9
Shaina <sdtalisay@up.edu.ph>
2024-12-12 12:43:15 +0800

    sidebar [Shaina]

--- src/pages/memberaboutus.tsx
+++ src/pages/memberaboutus.tsx
@@ -4,14 +4,14 @@ import MemberSidebar from "../components/membersidebar";
 const AboutUs = () => {
   return (
     <div className="flex"> 
-      <div className="sticky left-0 top-0 h-screen overflow-y-auto bg-white shadow-md">
+      <div className="sticky left-0 top-0 min-h-screen overflow-y-auto bg-white shadow-md">
 
       {/* Sidebar */}
       <MemberSidebar /> 
       </div>
 
       {/* Main content */}
-      <main className="main-content flex-grow p-6">
+      <main className="main-content flex-grow p-6 bg-white">
         <div className="flex flex-col items-center bg-gray-100 min-h-screen py-10 px-4">
           {/* Logo Section */}
           <div className="flex flex-col lg:flex-row items-center justify-center gap-8 bg-white p-8 rounded-lg shadow-md w-full max-w-6xl mb-12">
