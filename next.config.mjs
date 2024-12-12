6f3037e783b98f144513dd4335fa6f2b85219d9a
Shaina <sdtalisay@up.edu.ph>
2024-12-12 10:24:25 +0800

    ESlint ignore [Shaina]

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["firebasestorage.googleapis.com"]
-    }
-  };
+  images: {
+    domains: ["firebasestorage.googleapis.com"]
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+};
   
-  export default nextConfig;
+export default nextConfig;
